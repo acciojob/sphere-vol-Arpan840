78cfb223ee52f82d69a33599b39bcf38a995a9ae
Arpan840 <123809535+Arpan840@users.noreply.github.com>
2023-07-15 11:22:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function volume_sphere() {
     //Write your code here
+	let radious= document.getElementById('radius');
+     let r= radious.value;
+     let pi= 22/7;
+     let volume= 4/3*22/7*r*r*r;
+     let vol= document.getElementById('volume')
+     vol.value= volume
   
 } 
 
