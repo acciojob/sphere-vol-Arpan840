17b4dde3605328f2fd67c1bbf13a9bd4911da125
Arpan840 <123809535+Arpan840@users.noreply.github.com>
2023-07-19 23:58:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function volume_sphere(e) {
   let radius = document.getElementById('radius').value;
   let pi = 22 / 7;
   let volume = (4 / 3) * pi * radius * radius * radius;
-  let roundedVolume = volume.toFixed(2);
+  let roundedVolume = volume.toFixed(4);
   let vol = document.getElementById('volume');
   vol.value = roundedVolume;
   
